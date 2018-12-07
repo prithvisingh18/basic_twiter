886d3fdf7731286b80baae5c41612bff8cd0e41a
Prithvi Singh <prithvi.chandla@gmail.com>
2018-12-08 01:41:49 +0530

    Add files via upload

---
+++ postman_test/routes/index.js
@@ -0,0 +1,212 @@
+var express = require('express');
+var router = express.Router();
+const uuidv1 = require('uuid/v1');
+const _ = require('lodash');
+
+var dbInt = require('../models/dbInterface');
+
+
+// Register
+router.post('/register', async function (req, res, next) {
+  let response = { err: null, details: 'User Registered' };
+  try {
+    await dbInt.registerUser(req.body.username, req.body.password);
+  } catch (err) {
+    if (err.code === 'ER_DUP_ENTRY') {
+      response.err = 'DUP_USER';
+      response.details = 'User already exists';
+    }
+    console.log(err);
+  }
+  res.send(response);
+});
+
+//Login
+router.post('/login', async function (req, res, next) {
+  let response = { err: null, details: 'User Login Successfull' };
+  let login_res;
+  let sess = req.session;
+  try {
+    login_res = await dbInt.loginUser(req.body.username, req.body.password);
+  } catch (err) {
+    response.err = 'ERR';
+    response.details = 'Error While Login';
+    console.log(err);
+  }
+  if (login_res) {
+    sess.session_id = uuidv1();
+    sess.username = req.body.username;
+    res.send(response);
+  } else {
+    response.err = "Login failed";
+    response.details = "Wrong password or username";
+    res.send(response);
+  }
+});
+
+router.get('/logout', function (req, res) {
+  req.session.destroy(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/');
+    }
+  });
+
+});
+
+router.post('/follow', async function (req, res) {
+  let response = { err: null, details: "Followed" };
+  if (req.session.session_id) {
+    try {
+      await dbInt.followUser(req.body.followed, req.session.username);
+    } catch (err) {
+      if (err.code === 'ER_DUP_ENTRY') {
+        response.err = "DUP_FOLLOW";
+        response.details = "Already Followed";
+      } else {
+        response.err = "DB_ERR";
+        response.details = "Follow Failed";
+        console.log(err);
+      }
+    }
+  } else {
+    response.err = "SESSION_ERR";
+    response.details = "Login First"
+  }
+  res.send(response);
+});
+
+router.post('/unfollow', async function (req, res) {
+  let response = { err: null, details: "Unfollowed" };
+  if (req.session.session_id) {
+    try {
+      await dbInt.unFollowUser(req.body.followed, req.session.username);
+    } catch (err) {
+      response.err = "DB_ERR";
+      response.details = "Unfollow Failed";
+      console.log(err);
+    }
+  } else {
+    response.err = "SESSION_ERR";
+    response.details = "Login First"
+  }
+  res.send(response);
+});
+
+router.post('/tweet/create', async function (req, res) {
+  let response = { err: null, details: "Tweet Successfull" };
+  if (req.session.session_id) {
+    try {
+      await dbInt.createTweet(req.session.username, req.body.tweet);
+    } catch (err) {
+      response.err = "DB_ERR";
+      response.details = "Tweet Failed";
+      console.log(err);
+    }
+  } else {
+    response.err = "SESSION_ERR";
+    response.details = "Login First"
+  }
+  res.send(response);
+});
+
+router.post('/tweet/delete', async function (req, res) {
+  let response = { err: null, details: "Tweet Deleted" };
+  if (req.session.session_id) {
+    try {
+      await dbInt.deleteTweet(req.body.id);
+    } catch (err) {
+      response.err = "DB_ERR";
+      response.details = "Delete Failed";
+      console.log(err);
+    }
+  } else {
+    response.err = "SESSION_ERR";
+    response.details = "Login First"
+  }
+  res.send(response);
+});
+
+router.get('/tweet/get', async function (req, res) {
+  let response = { err: null, details: "Tweet Successfull", data: {} };
+  if (req.session.session_id) {
+    var tweets;
+    try {
+      tweets = await dbInt.getTweet(req.session.username);
+      response.data = tweets.map((x) => x);
+    } catch (err) {
+      response.err = "DB_ERR";
+      response.details = "Tweet Failed";
+      response.data = null;
+      console.log(err);
+    }
+  } else {
+    response.err = "SESSION_ERR";
+    response.details = "Login First"
+    response.data = null;
+  }
+  res.send(response);
+});
+
+router.post('/tweet/like', async function (req, res) {
+  let response = { err: null, details: "Tweet Liked", data: {} };
+  if (req.session.session_id) {
+    var tweets;
+    try {
+      await dbInt.like(req.body.tweet_op_id, req.session.username);
+    } catch (err) {
+      response.err = "DB_ERR";
+      response.details = "Like Failed";
+      response.data = null;
+      console.log(err);
+    }
+  } else {
+    response.err = "SESSION_ERR";
+    response.details = "Login First"
+    response.data = null;
+  }
+  res.send(response);
+});
+
+router.post('/tweet/unlike', async function (req, res) {
+  let response = { err: null, details: "Tweet Unliked", data: {} };
+  if (req.session.session_id) {
+    var tweets;
+    try {
+      await dbInt.unlike(req.body.tweet_op_id, req.session.username);
+    } catch (err) {
+      response.err = "DB_ERR";
+      response.details = "Unlike Failed";
+      response.data = null;
+      console.log(err);
+    }
+  } else {
+    response.err = "SESSION_ERR";
+    response.details = "Login First"
+    response.data = null;
+  }
+  res.send(response);
+});
+
+router.post('/tweet/retweet', async function (req, res) {
+  let response = { err: null, details: "Retweet Success", data: {} };
+  if (req.session.session_id) {
+    var tweets;
+    try {
+      await dbInt.retweet(req.body.tweet_op_id, req.session.username);
+    } catch (err) {
+      response.err = "DB_ERR";
+      response.details = "Retweet Failed";
+      response.data = null;
+      console.log(err);
+    }
+  } else {
+    response.err = "SESSION_ERR";
+    response.details = "Login First"
+    response.data = null;
+  }
+  res.send(response);
+});
+
+module.exports = router;
---
+++ postman_test/test/index.js
@@ -0,0 +1,88 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app');
+let should = chai.should();
+var expect = chai.expect;
+
+chai.use(chaiHttp);
+
+
+// describe('/POST register', () => {
+//     it('it should register user', (done) => {
+//         let data = {
+//             username: "test_1",
+//             password: "pass"
+//         }
+//         chai.request(server)
+//             .post('/register')
+//             .send(data)
+//             .end((err, res) => {
+//                 res.should.have.status(200);
+//                 res.body.should.be.a('object');
+//                 res.body.should.have.property('err');
+//                 done();
+//             });
+//     });
+
+// });
+
+describe('login', () => {
+    it('it should login', (done) => {
+        var agent = chai.request.agent(server)
+        agent
+            .post('/login')
+            .send({ username: 'test', password: 'test' })
+            .then(function (res) {
+                expect(res).to.have.status(200);
+                done();
+                // The `agent` now has the sessionid cookie saved, and will send it
+                // back to the server in the next request:
+                describe('GET Tweets', () => {
+                    it('it should get tweets', (done) => {
+                        agent.get('/tweet/get')
+                            .then(function (res) {
+                                expect(res).to.have.status(200);
+                                done();
+                            });
+                    });
+                });
+                describe('CREATE Tweets', () => {
+                    it('it should create tweets', (done) => {
+                        agent.post('/tweet/create')
+                            .send({ tweet: 'test_1234' })
+                            .then(function (res) {
+                                expect(res).to.have.status(200);
+                                done();
+                            });
+                    });
+                });
+                describe('DELETE Tweets', () => {
+                    it('it should delete tweets', (done) => {
+                        agent.post('/tweet/delete')
+                            .send({ id: '10003617-8b0e-57a8-8862-ba638224f35e' })
+                            .then(function (res) {
+                                expect(res).to.have.status(200);
+                            });
+                    });
+                });
+                describe('follow', () => {
+                    it('it should follow other user', (done) => {
+                        agent.post('follow')
+                            .send({ followed: 'test2' })
+                            .then(function (res) {
+                                expect(res).to.have.status(200);
+                            });
+                    });
+                });
+                describe('ufollow', () => {
+                    it('it should ufollow other user', (done) => {
+                        agent.post('unfollow')
+                            .send({ followed: 'test2' })
+                            .then(function (res) {
+                                expect(res).to.have.status(200);
+                            });
+                    });
+                });
+            });
+    });
+});
